abbbc2fc4f5236a8c94a644ef67d4d4d29603617
thirumalai kumar <sthirumalai@gmail.com>
2025-07-05 06:38:53 -0400

    * Leetcode - Binary Tree Preorder - 144

---
+++ leetcode/cpp/binarytreepreorder144.cpp
@@ -0,0 +1,75 @@
+// LeetCode - 144 - Binary Tree Preorder Traversal
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+// 
+// ex: root = [1, null, 2 ,3] => [1,2,3]
+// ex: root = [1,2,3,4,5,null,8,null,null,6,7,9] => [1,2,4,5,6,7,3,8,9]
+// ex: root = [] => []
+// ex: root = [1] => [1]
+//
+// Author: Thiru
+//
+// Time complexity: O(logn)
+// Space complexity: O(h) // height of the tree
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+class TreeNode {
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int d): data(d), left(nullptr), right(nullptr) {
+    }
+};
+
+std::vector<int> preorderTraversal(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    std::vector<int> vec{};
+
+    while(root || !stk.empty()) {
+        if (root) {
+            vec.push_back(root->data);
+            stk.push(root);
+            root = root->left;
+        } else {
+            root = stk.top();
+            stk.pop();
+            root = root->right;
+        }
+    }
+
+    return vec;
+}
+
+int main() {
+    TreeNode *root = new TreeNode(1);
+    root->left = nullptr;
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+    root->right->right = nullptr;
+
+    // TreeNode *root = new TreeNode(1);
+    // root->left = new TreeNode(2);
+    // root->right = new TreeNode(3);
+
+    // root->left->left = new TreeNode(4);
+    // root->left->right = new TreeNode(5);
+    // root->left->right->left = new TreeNode(6);
+    // root->left->right->right = new TreeNode(7);
+
+    // root->right->left = nullptr;
+    // root->right->right = new TreeNode(8);
+    // root->right->right->left = new TreeNode(9);
+    // root->right->right->right = nullptr;
+
+    std::vector<int> res = preorderTraversal(root);
+
+    for (const auto& r: res) {
+        std::cout<<r<<" ";
+    }
+    std::cout<<std::endl;
+
+    return 0;
+}
